b19dc8bf19c77814570b448d2c00f80dd88c83ba
Levi-21545 <lgomes.lrg@gmail.com>
2024-07-21 11:51:32 -0300

    refactor: Gambiarra que vai ser alterada de novo depois

--- src/app.js
+++ src/app.js
@@ -1,19 +1,11 @@
 const app = require('express')();
 const consign = require('consign');
 
-consign({ verbose: false })
-  .include('/src/config/middlewares.js')
+consign({ cwd: 'src', verbose: false })
+  .include('./config/middlewares.js')
+  .then('./routes')
+  .then('./config/router.js')
   .into(app);
-
-app.get('/users', (req, res) => {
-  const users = [{ name: 'John Doe', mail: 'john@mail.com' }];
-
-  res.status(200).json(users);
-});
-
-app.post('/users', (req, res) => {
-  res.status(201).json(req.body);
-});
 
 app.get('/', (req, res) => {
   res.status(200).send();
