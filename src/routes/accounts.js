89782953a6103b45f6cc17610dd9207c494e537b
Levi-21545 <lgomes.lrg@gmail.com>
2024-07-25 23:17:35 -0300

    refactor: Não retorna outro usuário

--- src/routes/accounts.js
+++ src/routes/accounts.js
@@ -13,7 +13,14 @@ module.exports = (app) => {
   router.get('/:id', (req, res, next) => {
     app.services.account
       .findById({ id: req.params.id })
-      .then((result) => res.status(200).json(result))
+      .then((result) => {
+        if (result.user_id !== req.user.id) {
+          return res
+            .status(403)
+            .json({ error: 'Este recurso não pertence ao usuário' });
+        }
+        res.status(200).json(result);
+      })
       .catch((err) => next(err));
   });
 
--- test/routes/account.test.js
+++ test/routes/account.test.js
@@ -93,7 +93,6 @@ test('Deve retornar uma conta por ID', () => {
       request(app)
         .get(`${MAIN_ROUTE}/${acc[0].id}`)
         .set('authorization', `bearer ${user.token}`)
-
         .then((res) => {
           expect(res.status).toBe(200);
           expect(res.body.name).toBe('Acc list');
@@ -102,7 +101,22 @@ test('Deve retornar uma conta por ID', () => {
     });
 });
 
-test.skip('Não deve retornar uma conta de outro usuário', () => {});
+test('Não deve retornar uma conta de outro usuário', () => {
+  return app
+    .db('accounts')
+    .insert({ name: 'Acc User #2', user_id: user2.id }, ['id'])
+    .then((acc) => {
+      request(app)
+        .get(`${MAIN_ROUTE}/${acc[0].id}`)
+        .set('authorization', `bearer ${user.token}`)
+        .then((res) => {
+          expect(res.status).toBe(403);
+          expect(res.body.error).toBe(
+            'Este recurso não pertence ao usuário'
+          );
+        });
+    });
+});
 
 test('Deve alterar uma conta', () => {
   return app
